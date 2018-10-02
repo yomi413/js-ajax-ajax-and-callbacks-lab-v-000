6d0c4086b8196c38fb7c6e561b752632886fc48b
Yomaira Escano <yjescano@gmail.com>
2018-10-02 02:44:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-$(document).ready(function (){
-});
+// $(document).ready(function (){
+// });
 
 function displayError() {
   $('#errors').html("I'm sorry, there's been an error. Please try again.")
