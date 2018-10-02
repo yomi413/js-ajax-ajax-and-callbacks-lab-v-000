51216b94bf35b02ded1bda51d13eb0dbc8049966
Yomaira Escano <yjescano@gmail.com>
2018-10-02 02:26:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // $(document).ready(function (){
 // });
 
-function searchRepositories() {
+// function searchRepositories() {
   // let search = $('#searchTerms').val()
   // $.get(`https://api.github.com/search/repositories?q=${search}`, data => {
   //     $('#results').html(renderSearchResults(data))
   //   }).fail(error => {
   //     displayError()
   //   })
-}
+// }
 
 // function renderSearchResult(result) {
 //   return
