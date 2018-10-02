1afe8d43587e0eb5ea449346d4aa5d7c45a2b535
Yomaira Escano <yjescano@gmail.com>
2018-10-02 02:46:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,18 +5,31 @@ function displayError() {
   $('#errors').html("I'm sorry, there's been an error. Please try again.")
 }
 
-const renderSearchResult = result => {
-  return `
-      <div>
-        <h2><a href="${result.html_url}">${result.name}</a></h2>
-        <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
-        <p>${result.description ? result.description : ''}</p>
-      </div>
-      <hr>
-    `;
+function renderSearchResult(result) {
+  return
+    `<div>
+      <h2><a href="${result.html_url}">${result.name}</a></h2>
+      <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
+      <p>${result.description}</p>
+    </div><hr>`
 }
 
-const renderSearchResults = data => data.items.map(result => renderSearchResult(result));
+function renderSearchResults(data) {
+  return data.items.map(result => renderSearchResult(result))
+}
+
+// const renderSearchResult = result => {
+//   return `
+//       <div>
+//         <h2><a href="${result.html_url}">${result.name}</a></h2>
+//         <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
+//         <p>${result.description ? result.description : ''}</p>
+//       </div>
+//       <hr>
+//     `;
+// }
+// 
+// const renderSearchResults = data => data.items.map(result => renderSearchResult(result));
 
 function searchRepositories() {
   let search = $('#searchTerms').val()
