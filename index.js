4a3dddd948b1c34c58fed05420536c0812c7dca7
Yomaira Escano <yjescano@gmail.com>
2018-10-02 02:25:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,15 +18,15 @@ function searchRepositories() {
 //       <p>${result.description}</p>
 //     </div><hr>`
 // }
-// 
+//
 // function renderSearchResults(data) {
 //   return data.items.map(result => renderSearchResult(result))
 // }
-// 
+//
 // function displayError() {
 //   $('#errors').html("I'm sorry, there's been an error. Please try again.")
 // }
-// 
+//
 // function showCommits(repo) {
 //   $.get(`https://api.github.com/repos/${repo.dataset.owner}/${repo.dataset.repository}/commits`, data => {
 //     $('#details').html(renderCommits(data))
@@ -34,12 +34,12 @@ function searchRepositories() {
 //     displayError()
 //   })
 // }
-// 
+//
 // function renderCommits(data) {
 //   let result = data.map(commit => renderCommit(commit)).join('')
 //   return `<ul>${result}</ul>`
 // }
-// 
+//
 // function renderCommit(commit) {
 //   return `<li><h3>${commit.sha}</h3><p>${commit.commit.message}</p></li>`
 // }
