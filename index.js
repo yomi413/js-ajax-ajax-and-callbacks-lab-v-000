5e499f7ba31e933c7d123dd88042af50ff3a23a2
Yomaira Escano <yjescano@gmail.com>
2018-10-02 01:11:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ function renderSearchResults(data) {
 
 function renderSearchResult(result) {
   return
-      `<div>
-        <h2><a href="${result.html_url}">${result.name}</a></h2>
-        <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
-        <p>${result.description ? result.description : ''}</p>
-      </div>`
+    `<div>
+      <h2><a href="${result.html_url}">${result.name}</a></h2>
+      <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
+      <p>${result.description ? result.description : ''}</p>
+    </div>`
 }
 
 function displayError() {
