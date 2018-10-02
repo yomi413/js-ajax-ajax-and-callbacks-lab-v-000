e256826f3aebb7cdca206e8923ac6bf75738a855
Yomaira Escano <yjescano@gmail.com>
2018-10-02 02:51:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,19 +5,6 @@ function displayError() {
   $('#errors').html("I'm sorry, there's been an error. Please try again.")
 }
 
-// function renderSearchResult(result) {
-//   return
-//     `<div>
-//       <h2><a href="${result.html_url}">${result.name}</a></h2>
-//       <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
-//       <p>${result.description}</p>
-//     </div><hr>`
-// }
-//
-// function renderSearchResults(data) {
-//   return data.items.map(result => renderSearchResult(result))
-// }
-
 const searchResult = result => {
   return `
     <div>
@@ -42,17 +29,17 @@ function searchRepositories() {
 
 function showCommits(repo) {
   $.get(`https://api.github.com/repos/${repo.dataset.owner}/${repo.dataset.repository}/commits`, data => {
-    $('#details').html(renderCommits(data))
+    $('#details').html(manyCommits(data))
   }).fail(error => {
     displayError()
   })
 }
 
-function renderCommits(data) {
-  let result = data.map(commit => renderCommit(commit)).join('')
+function manyCommits(data) {
+  let result = data.map(commit => oneCommit(commit)).join('')
   return `<ul>${result}</ul>`
 }
 
-function renderCommit(commit) {
+function oneCommit(commit) {
   return `<li><h3>${commit.sha}</h3><p>${commit.commit.message}</p></li>`
 }
