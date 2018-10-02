55039fbc1cef42d4a4aced87879a873de0fc1e5f
Yomaira Escano <yjescano@gmail.com>
2018-10-02 02:46:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ function displayError() {
   $('#errors').html("I'm sorry, there's been an error. Please try again.")
 }
 
-function renderSearchResult(result) {
-  return
-    `<div>
-      <h2><a href="${result.html_url}">${result.name}</a></h2>
-      <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
-      <p>${result.description}</p>
-    </div><hr>`
-}
-
-function renderSearchResults(data) {
-  return data.items.map(result => renderSearchResult(result))
-}
+// function renderSearchResult(result) {
+//   return
+//     `<div>
+//       <h2><a href="${result.html_url}">${result.name}</a></h2>
+//       <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
+//       <p>${result.description}</p>
+//     </div><hr>`
+// }
+// 
+// function renderSearchResults(data) {
+//   return data.items.map(result => renderSearchResult(result))
+// }
 
 // const renderSearchResult = result => {
 //   return `
