7f4d0c31ce815f9bc59f68acad5ea7bd9a6faf36
Yomaira Escano <yjescano@gmail.com>
2018-10-02 01:14:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ $(document).ready(function (){
 });
 
 function searchRepositories(searchTerms) {
-  let search = $('#searchTerms').val()
+  let search = $('#searchTerms')
 
   $.get(`https://api.github.com/search/repositories?q=${search}`, data => {
     $('#results').html(renderSearchResults(data))
